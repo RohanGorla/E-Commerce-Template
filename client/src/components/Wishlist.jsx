7308c56dfeef747f3a0fb7a2f095d711aa4d01e0
RohanGorla <rohangorla2010@gmail.com>
2024-09-08 13:44:09 +0530

    wishlist styling update

--- client/src/components/Wishlist.jsx
+++ client/src/components/Wishlist.jsx
@@ -41,38 +41,85 @@ function Wishlist() {
   }, []);
 
   return (
-    <div className="Wishlist_Main">
-      Wishlist
-      {/* add same features from cart */}
-      {wishlist.map((item, index) => {
-        return (
-          <div key={index}>
-            <img
-              src="https://cdn.thewirecutter.com/wp-content/media/2023/06/businesslaptops-2048px-0943.jpg"
-              style={{ width: "300px" }}
-            ></img>
-            <h2>{item.title}</h2>
-            <p>
-              {(item.price - item.price * (item.discount / 100)).toFixed(2)}
-            </p>
-            <button
-              onClick={() => {
-                addToCart(item);
-              }}
-            >
-              Add to cart
-            </button>
-            <button
-              onClick={() => {
-                removeFromWish(item.id);
-              }}
-            >
-              Remove
-            </button>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      {wishlist.length ? (
+        <div className="Wishlist_Main">
+          {wishlist.map((item, index) => {
+            return (
+              <div key={index} className="Wish_Item_Box">
+                <div className="Wish_Item">
+                  <div className="Wish_Item_Image">
+                    <img
+                      src="https://cdn.thewirecutter.com/wp-content/media/2023/06/businesslaptops-2048px-0943.jpg"
+                      style={{ width: "200px" }}
+                    ></img>
+                  </div>
+                  <div>
+                    <h2>{item.title}</h2>
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "start",
+                        marginTop: ".3em",
+                      }}
+                    >
+                      <p style={{ fontSize: "18px" }}>₹</p>
+                      <p style={{ fontSize: "20px" }}>
+                        {(
+                          item.price -
+                          item.price * (item.discount / 100)
+                        ).toFixed(2)}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="Wishlist_Buttons">
+                  <button
+                    onClick={() => {
+                      addToCart(item);
+                    }}
+                    style={{
+                      backgroundColor: "gold",
+                      borderStyle: "none",
+                      padding: "7px 10px",
+                      marginRight: "1em",
+                      borderRadius: "7px",
+                      fontSize: "15px",
+                      fontWeight: "600",
+                      cursor: "pointer",
+                    }}
+                  >
+                    Add to cart
+                  </button>
+                  <button
+                    onClick={() => {
+                      removeFromWish(item.id);
+                    }}
+                    style={{
+                      backgroundColor: "rgb(51, 50, 50)",
+                      color: "white",
+                      borderStyle: "none",
+                      padding: "7px 10px",
+                      borderRadius: "7px",
+                      fontSize: "15px",
+                      fontWeight: "600",
+                      cursor: "pointer",
+                      marginRight: "1em",
+                    }}
+                  >
+                    Remove
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div>
+          <h1 style={{ textAlign: "center" }}>Wishlist is empty!</h1>
+        </div>
+      )}
+    </>
   );
 }
 
