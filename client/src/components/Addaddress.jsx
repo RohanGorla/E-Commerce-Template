be62f3feab6b5922aeed4ae51a2178775d056350
RohanGorla <rohangorla2010@gmail.com>
2024-10-14 15:24:30 +0530

    add new layout for address page and update css for address an add address pages

--- client/src/components/Addaddress.jsx
+++ client/src/components/Addaddress.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../styles/Addaddress.css";
+import "../styles/Address.css";
 
 function Addaddress() {
   const [fullName, setFullName] = useState("");
@@ -12,11 +12,9 @@ function Addaddress() {
   const [state, setState] = useState("");
   const [country, setCountry] = useState("");
   const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-  // const [disable, setDisable] = useState(true);
   const navigate = useNavigate();
 
   async function addAddress() {
-    // const mail = localStorage.getItem("mailId");
     const mail = userInfo.mailId;
     let response = await axios.post("http://localhost:3000/addaddress", {
       mail: mail,
@@ -28,97 +26,97 @@ function Addaddress() {
       state: state,
       country: country,
     });
-    console.log(response);
     navigate("/account/address");
   }
 
   return (
-    <div className="Addaddress_Main">
-      <div className="Addaddress_Heading">
-        <h2>Add address</h2>
-      </div>
-      <div className="Addaddress_Component Addaddress_Name">
-        <label>Name on the address</label>
-        <input
-          type="text"
-          placeholder="Full name"
-          value={fullName}
-          onChange={(e) => {
-            setFullName(e.target.value);
-          }}
-          // id={disable ? "disable" : "active"}
-          // disabled={disable}
-        ></input>
-      </div>
-      <div className="Addaddress_Component Addaddress_House">
-        <label>House/Flat/Door.No</label>
-        <input
-          type="text"
-          placeholder="House/Flat/Door"
-          value={house}
-          onChange={(e) => {
-            setHouse(e.target.value);
-            // setDisable(false);
-          }}
-        ></input>
-      </div>
-      <div className="Addaddress_Component Addaddress_Street">
-        <label>Street</label>
-        <input
-          type="text"
-          placeholder="Street/Colony"
-          value={street}
-          onChange={(e) => {
-            setStreet(e.target.value);
-          }}
-        ></input>
-      </div>
-      <div className="Addaddress_Component Addaddress_Landmark">
-        <label>Landmark</label>
-        <input
-          type="text"
-          placeholder="Landmark/Nearby place"
-          value={landmark}
-          onChange={(e) => {
-            setLandmark(e.target.value);
-          }}
-        ></input>
-      </div>
-      <div className="Addaddress_Component Addaddress_Country">
-        <label>Country</label>
-        <input
-          type="text"
-          placeholder="Country"
-          value={country}
-          onChange={(e) => {
-            setCountry(e.target.value);
-          }}
-        ></input>
-      </div>
-      <div className="Addaddress_Component Addaddress_State">
-        <label>State</label>
-        <input
-          type="text"
-          placeholder="State"
-          value={state}
-          onChange={(e) => {
-            setState(e.target.value);
-          }}
-        ></input>
-      </div>
-      <div className="Addaddress_Component Addaddress_City">
-        <label>City</label>
-        <input
-          type="text"
-          placeholder="City"
-          value={city}
-          onChange={(e) => {
-            setCity(e.target.value);
-          }}
-        ></input>
-      </div>
-      <div className="Addaddress_Component Addaddress_Button">
-        <button onClick={addAddress}>Add address</button>
+    <div className="Addaddress_Page">
+      <div className="Addaddress_Main">
+        <div className="Addaddress_Main--Header">
+          <h2>Add address</h2>
+        </div>
+        <div className="Addaddress_Main--Inputs">
+          <div className="Addaddress_Component Addaddress_Name">
+            <label>Name on the address</label>
+            <input
+              type="text"
+              placeholder="Full name"
+              value={fullName}
+              onChange={(e) => {
+                setFullName(e.target.value);
+              }}
+            ></input>
+          </div>
+          <div className="Addaddress_Component Addaddress_House">
+            <label>House/Flat/Door.No</label>
+            <input
+              type="text"
+              placeholder="House/Flat/Door"
+              value={house}
+              onChange={(e) => {
+                setHouse(e.target.value);
+              }}
+            ></input>
+          </div>
+          <div className="Addaddress_Component Addaddress_Street">
+            <label>Street</label>
+            <input
+              type="text"
+              placeholder="Street/Colony"
+              value={street}
+              onChange={(e) => {
+                setStreet(e.target.value);
+              }}
+            ></input>
+          </div>
+          <div className="Addaddress_Component Addaddress_Landmark">
+            <label>Landmark</label>
+            <input
+              type="text"
+              placeholder="Landmark/Nearby place"
+              value={landmark}
+              onChange={(e) => {
+                setLandmark(e.target.value);
+              }}
+            ></input>
+          </div>
+          <div className="Addaddress_Component Addaddress_Country">
+            <label>Country</label>
+            <input
+              type="text"
+              placeholder="Country"
+              value={country}
+              onChange={(e) => {
+                setCountry(e.target.value);
+              }}
+            ></input>
+          </div>
+          <div className="Addaddress_Component Addaddress_State">
+            <label>State</label>
+            <input
+              type="text"
+              placeholder="State"
+              value={state}
+              onChange={(e) => {
+                setState(e.target.value);
+              }}
+            ></input>
+          </div>
+          <div className="Addaddress_Component Addaddress_City">
+            <label>City</label>
+            <input
+              type="text"
+              placeholder="City"
+              value={city}
+              onChange={(e) => {
+                setCity(e.target.value);
+              }}
+            ></input>
+          </div>
+          <div className="Addaddress_Component Addaddress_Button">
+            <button onClick={addAddress}>Add address</button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- client/src/components/Address.jsx
+++ client/src/components/Address.jsx
@@ -1,16 +1,39 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import "../styles/Address.css";
 import axios from "axios";
 
 function Address() {
+  const [orderedAddress, setOrderedAddress] = useState([]);
   const [address, setAddress] = useState([]);
+  console.log(address);
   const userInfo = JSON.parse(localStorage.getItem("userInfo"));
   const navigate = useNavigate();
+
+  async function getAddress() {
+    const mail = userInfo?.mailId;
+    let response = await axios.post("http://localhost:3000/getaddress", {
+      mail: mail,
+    });
+    if (response.data.access) {
+      setOrderedAddress(response.data.data);
+      let baseAddress = [];
+      let otherAddress = [];
+      response.data.data.filter((address) => {
+        if (address.addressname === userInfo?.base_address) {
+          baseAddress.push(address);
+        } else {
+          otherAddress.push(address);
+        }
+      });
+      setAddress([...baseAddress, ...otherAddress]);
+    }
+  }
 
   async function changeBaseAddress(current) {
     let response = await axios.put("http://localhost:3000/updatebaseaddress", {
       address: current,
-      mailId: userInfo.mailId,
+      mailId: userInfo?.mailId,
     });
     if (response.data.access) {
       localStorage.setItem(
@@ -18,128 +41,94 @@ function Address() {
         JSON.stringify({ ...userInfo, base_address: current.addressname })
       );
       localStorage.setItem("address", JSON.stringify(current));
+      let baseAddress = [];
+      let otherAddress = [];
+      orderedAddress.filter((address) => {
+        if (address.addressname === current.addressname) {
+          baseAddress.push(address);
+        } else {
+          otherAddress.push(address);
+        }
+      });
+      setAddress([...baseAddress, ...otherAddress]);
     }
   }
 
   useEffect(() => {
-    async function getAddress() {
-      const mail = userInfo.mailId;
-      let response = await axios.post("http://localhost:3000/getaddress", {
-        mail: mail,
-      });
-      if (response.data.access) {
-        setAddress(response.data.data);
-      }
-    }
     getAddress();
   }, []);
 
   return (
-    <div style={{ display: "flex", flexWrap: "wrap" }}>
-      <div
-        style={{
-          backgroundColor: "lightgrey",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          color: "black",
-          width: "350px",
-          height: "250px",
-          fontFamily: "monospace",
-          fontWeight: "600",
-          fontSize: "30px",
-          borderRadius: "20px",
-          margin: "20px 20px",
-          cursor: "pointer",
-        }}
+    <div className="Address_Page">
+      {/* <div
         onClick={() => {
           navigate("addaddress");
         }}
       >
         <p>+ ADD ADDRESS</p>
+      </div> */}
+      <div className="Address_Main">
+        <div className="Address_Main--Header">
+          <h1>Address</h1>
+        </div>
+        <div className="Address_Main--Addresses">
+          {address?.map((address, index) => {
+            return (
+              <div key={index} className="Address_Main--Address_Container">
+                <div
+                  className={
+                    userInfo?.base_address === address.addressname
+                      ? "Address_Main--Address Address_Main--Address--Default"
+                      : "Address_Main--Address"
+                  }
+                >
+                  <p className="Address_Main--Address_Name">
+                    {address.addressname}
+                    <span className="Address_Main--Address_Name--Default_Note">
+                      {userInfo?.base_address === address.addressname
+                        ? " (Default)"
+                        : ""}
+                    </span>
+                  </p>
+                  <p className="Address_Main--Address_Info">{address.house}</p>
+                  <p className="Address_Main--Address_Info">{address.street}</p>
+                  <p className="Address_Main--Address_Info">
+                    {address.landmark}
+                  </p>
+                  <p className="Address_Main--Address_Info">
+                    {address.city}, {address.state}
+                  </p>
+                  <p className="Address_Main--Address_Info">
+                    {address.country}
+                  </p>
+                  {address.addressname === userInfo?.base_address ? (
+                    <div className="Address_Main--Address_Options">
+                      <span className="Address_Main--Address_Options--Option">
+                        Remove
+                      </span>
+                    </div>
+                  ) : (
+                    <div className="Address_Main--Address_Options">
+                      <span className="Address_Main--Address_Options--Option">
+                        Remove
+                      </span>{" "}
+                      |{" "}
+                      <span
+                        className="Address_Main--Address_Options--Option"
+                        onClick={() => {
+                          changeBaseAddress(address);
+                        }}
+                      >
+                        Set as default
+                      </span>
+                    </div>
+                  )}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      {address?.map((address, index) => {
-        return (
-          <div
-            key={index}
-            style={{
-              backgroundColor: "lightgrey",
-              color: "black",
-              width: "350px",
-              height: "250px",
-              fontFamily: "monospace",
-              borderRadius: "20px",
-              margin: "20px 20px",
-              cursor: "pointer",
-            }}
-            onClick={() => {
-              const currentAddress = address;
-              console.log(currentAddress);
-              changeBaseAddress(currentAddress);
-            }}
-          >
-            <h2
-              style={{
-                fontSize: "30px",
-                textAlign: "center",
-                padding: "25px 0",
-              }}
-            >
-              {address.addressname}
-            </h2>
-            <p
-              style={{
-                fontSize: "14px",
-                fontWeight: "700",
-                margin: "10px 0",
-                marginLeft: "10px",
-              }}
-            >
-              {address.house}
-            </p>
-            <p
-              style={{
-                fontSize: "14px",
-                fontWeight: "700",
-                margin: "10px 0",
-                marginLeft: "10px",
-              }}
-            >
-              {address.street}
-            </p>
-            <p
-              style={{
-                fontSize: "14px",
-                fontWeight: "700",
-                margin: "10px 0",
-                marginLeft: "10px",
-              }}
-            >
-              {address.landmark}
-            </p>
-            <p
-              style={{
-                fontSize: "14px",
-                fontWeight: "700",
-                margin: "10px 0",
-                marginLeft: "10px",
-              }}
-            >
-              {address.city}, {address.state}
-            </p>
-            <p
-              style={{
-                fontSize: "14px",
-                fontWeight: "700",
-                margin: "10px 0",
-                marginLeft: "10px",
-              }}
-            >
-              {address.country}
-            </p>
-          </div>
-        );
-      })}
     </div>
   );
 }
