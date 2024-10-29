94a44b5d6527383e4109fd619f4e64e72064b7bd
RohanGorla <rohangorla2010@gmail.com>
2024-10-29 14:20:24 +0530

    Add Inventory management section in merchant dashboard

--- client/src/components/Merchant.jsx
+++ client/src/components/Merchant.jsx
@@ -15,19 +15,25 @@ function Merchant() {
             <div className="Merchant_Dashboard--Grid">
               <div className="Merchant_Dashboard--Item_Container">
                 <div className="Merchant_Dashboard--Item">
-                  <p className="Merchant_Dashboard--Item--Text">Pending</p>
+                  <p className="Merchant_Dashboard--Item--Text">
+                    Pending Orders
+                  </p>
                   <p className="Merchant_Dashboard--Item--Value">10</p>
                 </div>
               </div>
               <div className="Merchant_Dashboard--Item_Container">
                 <div className="Merchant_Dashboard--Item">
-                  <p className="Merchant_Dashboard--Item--Text">Shipped</p>
+                  <p className="Merchant_Dashboard--Item--Text">
+                    Shipped Orders
+                  </p>
                   <p className="Merchant_Dashboard--Item--Value">5</p>
                 </div>
               </div>
               <div className="Merchant_Dashboard--Item_Container">
                 <div className="Merchant_Dashboard--Item">
-                  <p className="Merchant_Dashboard--Item--Text">Delivered</p>
+                  <p className="Merchant_Dashboard--Item--Text">
+                    Finished Orders
+                  </p>
                   <p className="Merchant_Dashboard--Item--Value">26</p>
                 </div>
               </div>
@@ -50,6 +56,33 @@ function Merchant() {
                 <p>Inventory Management</p>
               </div>
             </div> */}
+            </div>
+          </div>
+          <div className="Merchant_Dashboard--Section">
+            <h2 className="Merchant_Dashboard--Section_Name">
+              Inventory Management
+            </h2>
+            <div className="Merchant_Dashboard--Grid">
+              <div className="Merchant_Dashboard--Item_Container">
+                <div className="Merchant_Dashboard--Item">
+                  <p className="Merchant_Dashboard--Item--Text">High Demand</p>
+                  <p className="Merchant_Dashboard--Item--Value">5</p>
+                </div>
+              </div>
+              <div className="Merchant_Dashboard--Item_Container">
+                <div className="Merchant_Dashboard--Item">
+                  <p className="Merchant_Dashboard--Item--Text">Out Of Stock</p>
+                  <p className="Merchant_Dashboard--Item--Value">2</p>
+                </div>
+              </div>
+              <div className="Merchant_Dashboard--Item_Container">
+                <div className="Merchant_Dashboard--Item">
+                  <p className="Merchant_Dashboard--Item--Text">
+                    Restock Needed
+                  </p>
+                  <p className="Merchant_Dashboard--Item--Value">34</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
