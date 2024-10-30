5cb3ac17c98f0449061e32ef2c95d93a190d66c2
RohanGorla <rohangorla2010@gmail.com>
2024-10-30 13:56:00 +0530

    Add styling for merchant login page

--- client/src/components/MerchantLogin.jsx
+++ client/src/components/MerchantLogin.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import "../styles/MerchantLogin.css";
 
 function MerchantLogin() {
   const [email, setEmail] = useState("");
