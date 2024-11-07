2a0acff7e68cc40571c4ec96523b4d7de128191e
RohanGorla <rohangorla2010@gmail.com>
2024-11-07 16:31:01 +0530

    Add new layout for Add Product Page

--- client/src/components/AddProduct.jsx
+++ client/src/components/AddProduct.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "../styles/AddProduct.css";
 
 function AddProduct() {
   const [file, setFile] = useState();
   const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
   const [discount, setDiscount] = useState("");
+  const [finalPrice, setFinalPrice] = useState("");
   const [productCat, setProductCat] = useState("");
   const [productCom, setProductCom] = useState("");
   const [actCat, setActCat] = useState("");
@@ -17,9 +20,8 @@ function AddProduct() {
   const [quantity, setQuantity] = useState("");
   const [stockAlert, setStockAlert] = useState("");
 
-  async function handleSubmit(e) {
+  async function handleSubmitProduct(e) {
     e.preventDefault();
-    // console.log(file.name, file.type);
     const response = await axios.post(
       `${import.meta.env.VITE_BASE_URL}/addproduct`,
       {
@@ -83,177 +85,195 @@ function AddProduct() {
   }, [fetch]);
 
   return (
-    <>
-      <p style={{ textAlign: "center" }}>Add Product</p>
-      <form
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-        onSubmit={handleSubmit}
-      >
-        <div style={{ margin: "1em 0" }}>
-          <label style={{ margin: "0 1em" }}>Photo</label>
-          <input
-            type="file"
-            onChange={(e) => {
-              setFile(e.target.files[0]);
-            }}
-          ></input>
+    <div className="AddProduct_Page">
+      <div className="AddProduct_Container">
+        <div className="AddProduct_Header">
+          <h1 className="AddProduct_Header--Title">Add Your Product</h1>
         </div>
-        <div style={{ margin: "1em 0" }}>
-          <label style={{ margin: "0 1em" }}>Title</label>
-          <input
-            type="text"
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-            value={title}
-          ></input>
+        <div className="AddProduct_Main">
+          {/* Product Details Form */}
+          <form className="AddProduct_Section" onSubmit={handleSubmitProduct}>
+            <div className="AddProduct--Basic_Details">
+              <div className="AddProduct--Basic_Details--Primary">
+                {/* Product Images Field */}
+                <div className="AddProduct_Section--Field">
+                  <label>Photo</label>
+                  <input
+                    type="file"
+                    onChange={(e) => {
+                      setFile(e.target.files[0]);
+                    }}
+                  ></input>
+                </div>
+                {/* Product Title Field */}
+                <div className="AddProduct_Section--Field">
+                  <label>Title</label>
+                  <textarea
+                    placeholder="Product name..."
+                    rows={2}
+                    value={title}
+                    onChange={(e) => {
+                      setTitle(e.target.value);
+                    }}
+                  ></textarea>
+                </div>
+                {/* Product Description Field */}
+                <div className="AddProduct_Section--Field">
+                  <label>Description</label>
+                  <textarea
+                    placeholder="Product description..."
+                    rows={4}
+                    value={description}
+                    onChange={(e) => {
+                      setDescription(e.target.value);
+                    }}
+                  ></textarea>
+                </div>
+              </div>
+              <div className="AddProduct--Basic_Details--Secondary">
+                {/* Product Price Field */}
+                <div className="AddProduct_Section--Field">
+                  <label>Price</label>
+                  <input
+                    type="number"
+                    onChange={(e) => {
+                      setPrice(e.target.value);
+                    }}
+                    value={price}
+                  ></input>
+                </div>
+                {/* Product Discount Field */}
+                <div className="AddProduct_Section--Field">
+                  <label>Discount</label>
+                  <input
+                    type="number"
+                    onChange={(e) => {
+                      setDiscount(e.target.value);
+                    }}
+                    value={discount}
+                  ></input>
+                </div>
+                {/* Product Final Price Field */}
+                <div className="AddProduct_Section--Field">
+                  <label>Final Price</label>
+                  <input
+                    type="number"
+                    onChange={(e) => {
+                      setFinalPrice(e.target.value);
+                    }}
+                    value={finalPrice}
+                  ></input>
+                </div>
+                {/* Product Category And Company Field */}
+                <div className="AddProduct_Category_And_Company">
+                  <div className="AddProduct_Section--Field">
+                    <label>Category</label>
+                    <select
+                      onChange={(e) => {
+                        setProductCat(e.target.value);
+                      }}
+                    >
+                      <option value="Select category" hidden>
+                        Select category
+                      </option>
+                      {allCat.map((cat, index) => {
+                        return (
+                          <option value={cat.ctegory} key={index}>
+                            {cat.category}
+                          </option>
+                        );
+                      })}
+                    </select>
+                  </div>
+                  <div className="AddProduct_Section--Field">
+                    <label>Company</label>
+                    <select
+                      onChange={(e) => {
+                        setProductCom(e.target.value);
+                      }}
+                    >
+                      <option value="Select company" hidden>
+                        Select company
+                      </option>
+                      {allCom.map((com, index) => {
+                        return (
+                          <option value={com.company} key={index}>
+                            {com.company}
+                          </option>
+                        );
+                      })}
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="AddProduct--Stock_Details">
+              <div className="AddProduct_Section--Field">
+                <label>Buy Limit Per Customer</label>
+                <input
+                  type="number"
+                  value={buyLimit}
+                  onChange={(e) => {
+                    setBuyLimit(e.target.value);
+                  }}
+                ></input>
+              </div>
+              <div className="AddProduct_Section--Field">
+                <label>Initial Stock Quantity</label>
+                <input
+                  type="number"
+                  value={quantity}
+                  onChange={(e) => {
+                    setQuantity(e.target.value);
+                  }}
+                ></input>
+              </div>
+              <div className="AddProduct_Section--Field">
+                <label>Stock alert</label>
+                <input
+                  type="number"
+                  value={stockAlert}
+                  onChange={(e) => {
+                    setStockAlert(e.target.value);
+                  }}
+                ></input>
+              </div>
+            </div>
+            <div className="AddProduct_Section--Submit">
+              <input type="submit" value="Add Product"></input>
+            </div>
+          </form>
+          <p>Add Category</p>
+          <form onSubmit={handleCategory}>
+            <div>
+              <label>Category</label>
+              <input
+                type="text"
+                onChange={(e) => {
+                  setActCat(e.target.value);
+                }}
+                value={actCat}
+              ></input>
+            </div>
+            <input type="submit"></input>
+          </form>
+          <p>Add Company</p>
+          <form onSubmit={handleCompany}>
+            <div>
+              <label>Company</label>
+              <input
+                type="text"
+                onChange={(e) => {
+                  setNewCompany(e.target.value);
+                }}
+                value={newCompany}
+              ></input>
+            </div>
+            <input type="submit"></input>
+          </form>
         </div>
-        <div style={{ margin: "1em 0" }}>
-          <label style={{ margin: "0 1em" }}>Price</label>
-          <input
-            type="text"
-            onChange={(e) => {
-              setPrice(e.target.value);
-            }}
-            value={price}
-          ></input>
-        </div>
-        <div style={{ margin: "1em 0" }}>
-          <label style={{ margin: "0 1em" }}>Disc</label>
-          <input
-            type="text"
-            onChange={(e) => {
-              setDiscount(e.target.value);
-            }}
-            value={discount}
-          ></input>
-        </div>
-        <div style={{ margin: "1em 0" }}>
-          <label style={{ margin: "0 1em" }}>Category</label>
-          {/* <input
-            type="text"
-            onChange={(e) => {
-              setProductCat(e.target.value);
-            }}
-            value={productCat}
-          ></input> */}
-          <select
-            onChange={(e) => {
-              setProductCat(e.target.value);
-            }}
-          >
-            <option value="Select category" hidden>
-              Select category
-            </option>
-            {allCat.map((cat, index) => {
-              return (
-                <option value={cat.ctegory} key={index}>
-                  {cat.category}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-        <div style={{ margin: "1em 0" }}>
-          <label style={{ margin: "0 1em" }}>Company</label>
-          <select
-            onChange={(e) => {
-              setProductCom(e.target.value);
-            }}
-          >
-            <option value="Select company" hidden>
-              Select company
-            </option>
-            {allCom.map((com, index) => {
-              return (
-                <option value={com.company} key={index}>
-                  {com.company}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-        <div style={{ margin: "1em 0" }}>
-          <label style={{ margin: "0 1em" }}>Buy limit</label>
-          <input
-            type="text"
-            value={buyLimit}
-            onChange={(e) => {
-              setBuyLimit(e.target.value);
-            }}
-          ></input>
-        </div>
-        <div style={{ margin: "1em 0" }}>
-          <label style={{ margin: "0 1em" }}>Quantity</label>
-          <input
-            type="text"
-            value={quantity}
-            onChange={(e) => {
-              setQuantity(e.target.value);
-            }}
-          ></input>
-        </div>
-        <div style={{ margin: "1em 0" }}>
-          <label style={{ margin: "0 1em" }}>Stock alert</label>
-          <input
-            type="text"
-            value={stockAlert}
-            onChange={(e) => {
-              setStockAlert(e.target.value);
-            }}
-          ></input>
-        </div>
-        <div>
-          <input type="submit"></input>
-        </div>
-      </form>
-      <p style={{ textAlign: "center", marginTop: "3em" }}>Add Category</p>
-      <form
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-        onSubmit={handleCategory}
-      >
-        <div style={{ margin: "1em 0" }}>
-          <label style={{ margin: "0 1em" }}>Category</label>
-          <input
-            type="text"
-            onChange={(e) => {
-              setActCat(e.target.value);
-            }}
-            value={actCat}
-          ></input>
-        </div>
-        <input type="submit"></input>
-      </form>
-      <p style={{ textAlign: "center", marginTop: "3em" }}>Add Company</p>
-      <form
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-        onSubmit={handleCompany}
-      >
-        <div style={{ margin: "1em 0" }}>
-          <label style={{ margin: "0 1em" }}>Company</label>
-          <input
-            type="text"
-            onChange={(e) => {
-              setNewCompany(e.target.value);
-            }}
-            value={newCompany}
-          ></input>
-        </div>
-        <input type="submit"></input>
-      </form>
-    </>
+      </div>
+    </div>
   );
 }
 
