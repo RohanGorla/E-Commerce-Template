3f55684b8ce9d7542db6fbd2656f3a9badd2d2b4
RohanGorla <rohangorla2010@gmail.com>
2024-11-17 15:15:42 +0530

    Add Error and success message boxes in Edit Merchant product page

--- client/src/components/MerchantEditProduct.jsx
+++ client/src/components/MerchantEditProduct.jsx
@@ -1,9 +1,45 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function MerchantEditProduct() {
-  return <div >
-
-  </div>;
+  const { productId } = useParams();
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  return (
+    <div className="AddProduct_Page">
+      {/* Error Message Box */}
+      <div
+        className={
+          error
+            ? "Error_Message_Box Error_Message_Box--Active"
+            : "Error_Message_Box Error_Message_Box--Inactive"
+        }
+      >
+        <div className="Error_Message_Box--Container">
+          <p className="Error_Message_Box--Heading">Error!</p>
+          <p className="Error_Message_Box--Message">{errorMessage}</p>
+        </div>
+      </div>
+      {/* Success Message Box */}
+      <div
+        className={
+          success
+            ? "Success_Message_Box Success_Message_Box--Active"
+            : "Success_Message_Box Success_Message_Box--Inactive"
+        }
+      >
+        <div className="Success_Message_Box--Container">
+          <p className="Success_Message_Box--Heading">Success!</p>
+          <p className="Success_Message_Box--Message">{successMessage}</p>
+        </div>
+      </div>
+      <div className="AddProduct_Container">
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default MerchantEditProduct;
