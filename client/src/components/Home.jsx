b7f0c8e7618893e851a713d87408ce4573011649
RohanGorla <rohangorla2010@gmail.com>
2024-11-28 23:48:05 +0530

    Add store name and shop now button in home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,6 +6,11 @@ function Home() {
   return (
     <div className="Home_Page">
       <div className="Home_Header">
+        <div className="Home_Title">
+          <h1>E-Commerce Store</h1>
+          <p>For all your needs and more!</p>
+          <button>Shop now</button>
+        </div>
         <div className="Home_BG--Image">
           <img src={BGImg}></img>
         </div>
