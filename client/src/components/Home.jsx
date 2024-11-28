c0896aaba23c832ebc22019daa732aaf8585a49c
RohanGorla <rohangorla2010@gmail.com>
2024-11-29 01:25:28 +0530

    Add get home page products function to get various type of products info to home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,19 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import BGImg from "../assets/lady_bg_2.jpg";
 import "../styles/Home.css";
 
 function Home() {
+  const [products, setProducts] = useState([]);
+
+  async function getMostBought() {
+    const response = await axios.get(
+      `${import.meta.env.VITE_BASE_URL}/gethomeproducts`
+    );
+    if (response.data.access) {
+      setProducts(response.data.data);
+    }
+  }
   return (
     <div className="Home_Page">
       <div className="Home_Header">
@@ -14,6 +25,9 @@ function Home() {
         <div className="Home_BG--Image">
           <img src={BGImg}></img>
         </div>
+      </div>
+      <div className="Home_Main">
+        <button onClick={getMostBought}>get products</button>
       </div>
     </div>
   );
