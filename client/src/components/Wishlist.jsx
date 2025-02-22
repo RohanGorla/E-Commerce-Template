3cdbeed79f8216c4e5f86881518c4e13ab697302
RohanGorla <rohangorla2010@gmail.com>
2025-02-22 12:19:19 +0530

    Add the get image urls function in wishlist page

--- client/src/components/Wishlist.jsx
+++ client/src/components/Wishlist.jsx
@@ -22,6 +22,17 @@ function Wishlist() {
   const mailId = userInfo?.mailId;
   const navigate = useNavigate();
 
+  /* GET PRODUCT IMAGE URLS FROM S3 */
+
+  async function getImageUrls(imageKeys) {
+    const getUrlResponse = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/generategeturls`,
+      { imageKeys: [imageKeys[0]] }
+    );
+    const getUrls = getUrlResponse.data;
+    return getUrls;
+  }
+
   /* Wish Lists APIs */
 
   async function getWishlists() {
