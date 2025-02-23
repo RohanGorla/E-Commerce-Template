91fa06a94215759b00c09a75268da9eeaf067a50
RohanGorla <rohangorla2010@gmail.com>
2025-02-24 01:55:43 +0530

    Add the error and success message boxes in merchant edit company page

--- client/src/components/MerchantEditCompany.jsx
+++ client/src/components/MerchantEditCompany.jsx
@@ -4,9 +4,40 @@ import "../styles/MerchantEditDetails.css";
 function MerchantEditCompany() {
   const merchantInfo = JSON.parse(localStorage.getItem("merchantInfo"));
   const [newCompany, setNewCompany] = useState(merchantInfo.company);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
 
   return (
     <div className="MerchantEditCompany_Page">
+      {/* Error Message Box */}
+      <div
+        className={
+          error
+            ? "Error_Message_Box Error_Message_Box--Active"
+            : "Error_Message_Box Error_Message_Box--Inactive"
+        }
+      >
+        <div className="Error_Message_Box--Container">
+          <p className="Error_Message_Box--Heading">Error!</p>
+          <p className="Error_Message_Box--Message">{errorMessage}</p>
+        </div>
+      </div>
+      {/* Success Message Box */}
+      <div
+        className={
+          success
+            ? "Success_Message_Box Success_Message_Box--Active"
+            : "Success_Message_Box Success_Message_Box--Inactive"
+        }
+      >
+        <div className="Success_Message_Box--Container">
+          <p className="Success_Message_Box--Heading">Success!</p>
+          <p className="Success_Message_Box--Message">{successMessage}</p>
+        </div>
+      </div>
+
       <div className="MerchantEditDetail_Main">
         <div className="MerchantEditDetail_Header">
           <h2>Change your company name</h2>
