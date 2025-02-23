d0333de25d38abbe2c5b623cfbacbaae34bafeca
RohanGorla <rohangorla2010@gmail.com>
2025-02-23 20:32:18 +0530

    Add the show merchant details labels and information in merchant details page

--- client/src/components/MerchantDetails.jsx
+++ client/src/components/MerchantDetails.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import "../styles/MerchantDetails.css";
 
 function MerchantDetails() {
+  const merchantInfo = JSON.parse(localStorage.getItem("merchantInfo"));
+
   return (
     <div className="MerchantDetails_Page">
       <div className="MerchantDetails_Container">
@@ -10,10 +12,18 @@ function MerchantDetails() {
         </div>
         <div className="MerchantDetails_Content">
           <div className="MerchantDetails_Section">
+            <p className="MerchantDetails_Labels">Company</p>
+            <p className="MerchantDetails_Information">
+              {merchantInfo.company}
+            </p>
           </div>
           <div className="MerchantDetails_Section">
+            <p className="MerchantDetails_Labels">Email</p>
+            <p className="MerchantDetails_Information">{merchantInfo.mailId}</p>
           </div>
           <div className="MerchantDetails_Section">
+            <p className="MerchantDetails_Labels">Password</p>
+            <p className="MerchantDetails_Information">********</p>
           </div>
         </div>
       </div>
