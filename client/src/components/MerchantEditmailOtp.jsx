df840be64c2096d86f834217d684d0027ccdd422
RohanGorla <rohangorla2010@gmail.com>
2025-02-24 19:07:10 +0530

    Add the error and success message boxes in merchant email edit otp page

--- client/src/components/MerchantEditmailOtp.jsx
+++ client/src/components/MerchantEditmailOtp.jsx
@@ -4,6 +4,10 @@ import { useOutletContext } from "react-router-dom";
 function MerchantEditmailOtp() {
   const context = useOutletContext();
   const [OTP, setOTP] = useState("");
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
 
   async function checkotp() {
     const checkOtpResponse = await axios.post(
@@ -17,6 +21,33 @@ function MerchantEditmailOtp() {
 
   return (
     <div className="MerchantEmailEditOtp_Page">
+      {/* Error Message Box */}
+      <div
+        className={
+          error
+            ? "Error_Message_Box Error_Message_Box--Active"
+            : "Error_Message_Box Error_Message_Box--Inactive"
+        }
+      >
+        <div className="Error_Message_Box--Container">
+          <p className="Error_Message_Box--Heading">Error!</p>
+          <p className="Error_Message_Box--Message">{errorMessage}</p>
+        </div>
+      </div>
+      {/* Success Message Box */}
+      <div
+        className={
+          success
+            ? "Success_Message_Box Success_Message_Box--Active"
+            : "Success_Message_Box Success_Message_Box--Inactive"
+        }
+      >
+        <div className="Success_Message_Box--Container">
+          <p className="Success_Message_Box--Heading">Success!</p>
+          <p className="Success_Message_Box--Message">{successMessage}</p>
+        </div>
+      </div>
+      {/* Enter Otp To Edit Merchant Email */}
       <div className="MerchantEditDetail_Main">
         <div className="MerchantEditDetail_Header">
           <h2>Enter OTP.</h2>
